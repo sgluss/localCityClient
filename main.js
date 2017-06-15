be24778cdfc353e20ab9951ef3418ed3149a8225
APOPHIS\sam <sgluss@outlook.com>
2017-06-15 08:11:48 -0700

    now hooked to EC2 server

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 (function () {
 	var mc = window.mapController = window.mapController || {};
 	
-	var serverURL = "http://randomDomain.com:5000/";
+	var serverURL = "http://52.34.170.97:5000/";
 	
 	var map;
 	var markers = [];
